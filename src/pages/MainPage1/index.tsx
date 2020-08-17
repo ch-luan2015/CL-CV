45e386fa1dcf06632d4a560f6f9d91bdc6d22aca
ch-luan2015 <ch.luan2015@gmail.com>
2020-08-18 01:49:52 +0700

    Layout new

--- src/pages/MainPage1/index.tsx
+++ src/pages/MainPage1/index.tsx
@@ -7,7 +7,7 @@ interface Props {}
 function MainPage1(props: Props) {
   return (
     <div className="p-6 sm:p-10 md:p-16 flex flex-wrap">
-        <LeftBar />
+      <LeftBar />
       <div className="w-full md:w-1/2 order-1 md:order-2">
         <div className="max-w-md leading-loose tracking-tight">
           <Body />
