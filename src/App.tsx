b401a09547ed132ddf63d69f86dfd73742db2111
ch-luan2015 <45224298+ch-luan2015@users.noreply.github.com>
2020-08-18 21:15:25 +0700

    Add firebase

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,31 @@
-import React from "react";
-import "./App.scss"
+import React from 'react'
+import './App.scss'
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom'
 
-import Layout from "./layout";
-import MainPage1 from "./pages/MainPage1"
+import Layout from './layout'
+import MainPage1 from './pages/MainPage1'
+import HomePage from './pages/HomePage'
+import Post from './components/Post'
+import NoMatch from './components/NoMatch'
 
 function App() {
-
-
   return (
     <Layout>
-      <MainPage1 />
+      <Router>
+        <nav>
+          <Link to="/">
+            <h2>My React + Firebase Blog</h2>
+          </Link>
+        </nav>
+        <main>
+          <Switch>
+            <Route exact path="/" component={HomePage} />
+            <Route path="/404" component={NoMatch} />
+            <Route path="/:slug" component={Post} />
+          </Switch>
+        </main>
+      </Router>
+      {/* <MainPage1 /> */}
     </Layout>
   )
 }
---
+++ src/firebase.js
@@ -0,0 +1,27 @@
+import firebase from "firebase/app";
+import database from "firebase/database";
+
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAchFAZua_n64MblMmppie1hRFeC7UsL7M",
+    authDomain: "clblog123-a057e.firebaseapp.com",
+    databaseURL: "https://clblog123-a057e.firebaseio.com",
+    projectId: "clblog123-a057e",
+    storageBucket: "clblog123-a057e.appspot.com",
+    messagingSenderId: "471058302854",
+    appId: "1:471058302854:web:98e6eaefd1d5092d8c4e07"
+  };
+
+
+  let firebaseCache;
+
+  export const getFirebase = () => {
+    if (firebaseCache) {
+      return firebaseCache;
+    }
+  
+    firebase.initializeApp(config);
+    firebaseCache = firebase;
+    return firebase;
+  };
